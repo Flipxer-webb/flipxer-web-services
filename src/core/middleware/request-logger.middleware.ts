dd422295a1c2cde73a388dfc4c02f626b7fd8dd6
Eyihbanner <magpiep18@gmail.com>
2026-01-04 20:26:25 +0000

    feat: add request logging middleware to log HTTP requests and responses.

---
+++ src/core/middleware/request-logger.middleware.ts
@@ -0,0 +1,25 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { Logger } from '@nestjs/common';
+
+@Injectable()
+export class RequestLoggerMiddleware implements NestMiddleware {
+    private logger = new Logger('HTTP');
+
+    use(req: Request, res: Response, next: NextFunction) {
+        const { method, originalUrl } = req;
+        const start = Date.now();
+
+        res.on('finish', () => {
+            const { statusCode } = res;
+            const duration = Date.now() - start;
+
+            // Log with distinct prefix for easy filtering
+            this.logger.log(
+                `[API_HIT] ${method} ${originalUrl} ${statusCode} +${duration}ms`
+            );
+        });
+
+        next();
+    }
+}
