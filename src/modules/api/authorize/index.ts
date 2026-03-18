0c08e6bfa3826222a966e988dab80134ea43605b
Flipxer-webb <hello@flipxer.com>
2026-03-18 08:01:26 +0000

    fix(auth): import session module in authorize module

--- src/modules/api/authorize/index.ts
+++ src/modules/api/authorize/index.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import AuthorizationService from "./services/authorize.service";
 import AuthorizationController from "./controllers/v1";
+import { AuthModule } from "../auth";
+import { SessionModule } from "../session";
 
 @Module({
+    imports: [AuthModule, SessionModule],
     providers: [AuthorizationService],
     controllers: [AuthorizationController],
 })
